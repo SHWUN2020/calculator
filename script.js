dcb6a58b57ecaa4fe999acfb67261d495786c781
SHWUN2020 <128058715+SHWUN2020@users.noreply.github.com>
2025-10-27 23:47:00 +0330

    Standardizing styling using CSS Grid writing basic input function

---
+++ script.js
@@ -0,0 +1,10 @@
+const inputBox = document.getElementById('segment');
+
+function fun(active, idset){
+    alert(`${active} ${idset}`);
+    let equal = ['']
+}
+
+function num(number){
+    alert(number);
+}
