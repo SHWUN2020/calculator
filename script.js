f6216ed825c75b3750ff3a0da8be25ace8d18ba8
SHWUN2020 <128058715+SHWUN2020@users.noreply.github.com>
2025-10-28 23:39:29 +0330

    Ability to enter numbers and symbols no functionality

--- script.js
+++ script.js
@@ -1,10 +1,34 @@
 const inputBox = document.getElementById('segment');
+const backBox = document.getElementById('base-segment');
 
-function fun(active, idset){
-    alert(`${active} ${idset}`);
-    let equal = ['']
+
+
+if (inputBox.style.width > backBox.style.width) {
+    alert('max value');
+    inputBox.style.fontSize / 1.5;
 }
 
-function num(number){
-    alert(number);
+function fun(active, idset) {
+    let equal = ['clear', '/', '×', '+', '-', ''];
+
+    if (active == true && !(idset == undefined)) {
+        // alert(`${active} ${idset}`);
+        if (!(inputBox.value == '0' || idset == 0)) {
+            inputBox.value += equal[idset]
+        } else if (idset == 0) {
+            inputBox.value = 0; 
+        }
+    }
+    active = false
 }
+
+function num(number) {
+    // alert(number);
+    if (inputBox.value == '0') {
+        // inputBox.innerHTML = Number();
+        inputBox.value = number;
+    } else {
+        inputBox.value += number;
+    }
+}
+
