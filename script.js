3de9ec31fd58a420f080bd93314eb120bddca7db
SHWUN2020 <128058715+SHWUN2020@users.noreply.github.com>
2025-10-29 22:58:13 +0330

    Ability to calculator integers and simple oprations

--- script.js
+++ script.js
@@ -1,34 +1,79 @@
 const inputBox = document.getElementById('segment');
+const inputBox2 = document.getElementById('segment2');
+const inputBox3 = document.getElementById('segment3');
 const backBox = document.getElementById('base-segment');
+let checkValue = false, saveMemory = 0, saveOp = 0;
 
 
 
-if (inputBox.style.width > backBox.style.width) {
-    alert('max value');
-    inputBox.style.fontSize / 1.5;
-}
-
-function fun(active, idset) {
-    let equal = ['clear', '/', '×', '+', '-', ''];
-
-    if (active == true && !(idset == undefined)) {
+function fun(active, OP, idset) {
+    let equal = ['clear', '/', 'x', '+', '-', ''], x, Memory = idset;
+    if (active == true && !(idset == undefined) && !(Memory == undefined || Memory == null)) {
         // alert(`${active} ${idset}`);
-        if (!(inputBox.value == '0' || idset == 0)) {
-            inputBox.value += equal[idset]
-        } else if (idset == 0) {
-            inputBox.value = 0; 
+        if (!(inputBox.innerHTML == '0' || OP == 0)) {
+            inputBox2.innerHTML += equal[OP];
+            checkValue = true;
+            // alert(OpFix);
+            if(idset == 5){
+                switch (OP) {
+                case 1:
+                    saveMemory = inputBox.innerHTML / inputBox3.innerHTML;
+                    console.log('/')
+                    break;
+                case 2:
+                    saveMemory = inputBox.innerHTML * inputBox3.innerHTML;
+                    console.log('X');
+                    break;
+                case 3:
+                    saveMemory += Number(inputBox.innerHTML) + Number(inputBox3.innerHTML);
+                    console.log('+');
+                    break;
+                case 4:
+                    saveMemory = inputBox.innerHTML - inputBox3.innerHTML;
+                    console.log('-');
+                    break;
+                default:
+                    console.log('Error system')
+                    break;
+            }
+            
+            console.log('nothing Oparator Option...!' + idset);
+            console.log(inputBox.innerHTML);
+            console.log(inputBox3.innerHTML);
+            x = Number(inputBox.innerHTML);
+            inputBox.innerHTML = saveMemory;
+            inputBox2.innerHTML = '';
+            inputBox3.innerHTML = '';
+            console.log(saveMemory);
+            checkValue = false;
+            }
+
+        } else if (active == true && idset == 0) {
+            inputBox.innerHTML = 0;
+            inputBox2.innerHTML = '';
+            inputBox3.innerHTML = '';
+            checkValue = false;
         }
     }
-    active = false
 }
 
 function num(number) {
     // alert(number);
-    if (inputBox.value == '0') {
-        // inputBox.innerHTML = Number();
-        inputBox.value = number;
+    if (checkValue == false) {
+        if (inputBox.innerHTML == '0') {
+            // inputBox.innerHTML.innerHTML = Number();
+            inputBox.innerHTML = `${number}`;
+            console.log('checked')
+        } else {
+            inputBox.innerHTML += `${number}`;
+        }
     } else {
-        inputBox.value += number;
+        if (inputBox3.innerHTML == '0') {
+            // inputBox.innerHTML.innerHTML = Number();
+            inputBox3.innerHTML = `${number}`;
+        } else {
+            inputBox3.innerHTML += `${number}`;
+        }
     }
 }
 
